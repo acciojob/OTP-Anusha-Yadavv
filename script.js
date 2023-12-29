fc4511af2016541140f9c8a1338c33f187b232e2
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-29 17:03:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function focusNext(input) {
+            const maxLength = parseInt(input.getAttribute('maxlength'));
+            const currentLength = input.value.length;
+
+            if (currentLength >= maxLength) {
+                const nextInput = input.nextElementSibling;
+                if (nextInput) {
+                    nextInput.focus();
+                }
+            }
+
+            if (currentLength === 0) {
+                const prevInput = input.previousElementSibling;
+                if (prevInput) {
+                    prevInput.focus();
+                }
+            }
+        }
